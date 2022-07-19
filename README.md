5683f059c2c69a4e41eca0cf87d6912cfb7cbb1c
Karl Reype <genuisxl@gmail.fr>
2022-07-19 20:18:15 +0200

    ajout de l'application store

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Marketplace
 Mon projet sert de tester mes compétences en créant un site e-commerce avec une deadline datant du 28/07
+
+Connection serveur administrateur:
+user: admin
+psw: admin123
+
---
+++ marketplace/src/store/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+Product
+- Nom (char)
+- Prix (float / integer)
+- Stock (integer)
+- Description (text)
+- Image (image)
+
+"""
+
+class Product (models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}  ({self.stock})"
