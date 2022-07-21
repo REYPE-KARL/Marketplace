4dccafddee95bc22f290bf976e93f796dd7b5e42
Karl Reype <genuisxl@gmail.fr>
2022-07-21 20:22:10 +0200

    ajout des pages login et contact

--- marketplace/src/accounts/views.py
+++ marketplace/src/accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth import get_user_model, login, logout, authenticate
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 User = get_user_model()
@@ -9,13 +10,35 @@ def signup(request):
         #Traiter la requete
         username = request.POST.get("username")
         password = request.POST.get("password")
-        user = User.objects.create_user(username="username", password="password")
+        user = User.objects.create_user(username=username, password=password)
 
         login(request, user)
         return redirect('index')
 
     return render(request, 'accounts/signup.html')
 
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+
+    return render(request, 'accounts/login.html')
+
+
 def logout_user(request):
     logout(request)
     return redirect('index')
+
+def message_contact(request):
+    # return HttpResponse("Hello contact's page")
+    if request.method == "POST":
+        frame = "oui"
+        return HttpResponse(f'Hello { frame }')
+
+    return render(request, 'accounts/contact.html')
